8450904dc7e37dafd949c26a49c291a76ab8c7c6
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2022-03-27 15:17:23 +0200

    Various

--- src/MediaPlayer/MediaInfo/MediaInfo.js
+++ src/MediaPlayer/MediaInfo/MediaInfo.js
@@ -1,16 +1,19 @@
 
 import React, { useState } from 'react';
 import './mediainfo.css';
+import { MdOutlineIosShare as Share } from 'react-icons/md';
 
 export default function MediaInfo() {
     return (
         <div className='media-info'>
             <div className='info'>
-                <div className='title'>Title</div>
-                <div className='artist'>Artist</div>
+                <div className='title'>Human</div>
+                <div className='artist'>One Republic</div>
             </div>
             <div className='share'>
-                <div className='share-title'>Share</div>
+                <div className='share-title'>
+                    <Share size={20} />
+                    </div>
             </div>
         </div>
     )
