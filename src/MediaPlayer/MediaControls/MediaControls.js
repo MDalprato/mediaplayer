48bcd1967ae2cc46ebfa87fedce685d4e37d57b3
Marco Dalprato <5076292+MDalprato@users.noreply.github.com>
2022-03-27 18:17:20 +0200

    Styling ...

--- src/MediaPlayer/MediaControls/MediaControls.js
+++ src/MediaPlayer/MediaControls/MediaControls.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import './mediacontrols.css';
-import { BsPlayFill as Play } from 'react-icons/bs';
-import { BsStopFill as Stop } from 'react-icons/bs';
+import { GrPlay as Play } from 'react-icons/gr';
+import { GrStop as Stop } from 'react-icons/gr';
+import { GrNext as Next } from 'react-icons/gr';
+
 import RangeSlider from 'react-bootstrap-range-slider';
+import ProgressBar from "@ramonak/react-progress-bar";
 
 export default function MediaControls() {
 
@@ -10,26 +13,36 @@ export default function MediaControls() {
     const [volume, setVolume] = useState(0);
 
     return (
-        
- 
+
+
         <div className='media-controls'>
             <div className='seek-bar'>
-                <RangeSlider
-                    tooltip={'off'}
-                    value={seek}
-                    onChange={changeEvent => setSeek(changeEvent.target.value)}
+
+                <ProgressBar
+                    completed={80}
+                    className="wrapper"
+                    barContainerClassName="container"
+                    completedClassName="barCompleted"
+                    labelClassName="label"
                 />
+                <div className='from'>00:00</div>
+                <div className='to'>03:00</div>
+
             </div>
             <div className='buttons'>
-                <Play size={40} />
-                <Stop size={40} />
+                <Play className='play' size={40} />
+                {/* <Stop size={40} /> */}
+                <Next className='next' size={40} />
+                <Next className='previous' size={40} />
 
             </div>
             <div className='volue-control'>
                 <div className='volume-slider'>
                     <RangeSlider
+                        size='sm'
                         tooltip={'off'}
                         value={volume}
+                        variant='primary'
                         onChange={changeEvent => setVolume(changeEvent.target.value)}
                     />
                 </div>
